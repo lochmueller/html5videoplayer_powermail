15a50e1b815ac89a2bcb55e3efa33e4b11f5a99b
Tim Lochmüller <tim@fruit-lab.de>
2016-06-16 19:13:35 +0200

    Add first Test class

---
+++ Tests/Unit/Service/SessionServiceTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SessionServiceTest
+ */
+
+namespace FRUIT\Html5videoplayerPowermail\Tests\Unit\Service;
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * SessionServiceTest
+ */
+class SessionServiceTest extends UnitTestCase
+{
+
+}
